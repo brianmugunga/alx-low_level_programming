69b0ad430e8e5ac51bf82f932937c179f53ab7ec
brianmugunga <104896959+brianmugunga@users.noreply.github.com>
2022-09-02 08:24:51 +0300

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,20 +1,13 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
-/*
- * File: lists.h
- * Auth: Brennan D Baraban
- */
-
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
- *
  * Description: doubly linked list node structure
  * for Holberton project
  */
@@ -35,4 +28,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif /* LISTS_H */
+#endif /* _LISTS_H_ */
